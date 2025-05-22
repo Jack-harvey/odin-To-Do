0dc210d78e1899aa240fb21d39c5b95e947b6ffc
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-05-22 17:02:46 +1000

    Style project cards

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import "../components/style/style.css";
 import { PubSub } from "../api/pubsub";
+import { createProjectCard } from "../features/projects/scripts/renderProjects";
 import {
   create,
   read,
@@ -8,8 +9,18 @@ import {
   add,
   getListOfToDos,
   checkIfLocalStorageDataExists,
+  getCountOfAllTodosOnProject,
+  getCountOfAllCompletedTodosOnProject,
 } from "../database/ls";
 import { Project } from "../features/projects/scripts/project";
 import { todo } from "../features/toDos/scripts/todo";
 
 checkIfLocalStorageDataExists();
+console.log(getCountOfAllTodosOnProject("46bb5ae4-dba6-41b1-b28e-e83b0c913347"));
+console.log(getCountOfAllCompletedTodosOnProject("46bb5ae4-dba6-41b1-b28e-e83b0c913347"));
+
+const proj = JSON.parse(localStorage.getItem("projects"))[0];
+
+const x = createProjectCard(proj);
+
+document.querySelector(".project-card-area").appendChild(x);
