46968b8480f07a87e072577cc0c34fecf265f32d
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-06-03 20:13:20 +1000

    Add projects on load

--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,5 @@
 import "../components/style/style.css";
-import { PubSub } from "../api/pubsub";
-import { createProjectCard } from "../features/projects/scripts/renderProjects";
-import { renderTodos } from "../features/toDos/scripts/renderTodos";
-import {
-  create,
-  read,
-  update,
-  remove,
-  add,
-  getListOfToDos,
-  checkIfLocalStorageDataExists,
-  getCountOfAllTodosOnProject,
-  getCountOfAllCompletedTodosOnProject,
-} from "../database/ls";
-import { Project } from "../features/projects/scripts/project";
-import { todo } from "../features/toDos/scripts/todo";
 
-checkIfLocalStorageDataExists();
-console.log(getCountOfAllTodosOnProject("46bb5ae4-dba6-41b1-b28e-e83b0c913347"));
-console.log(getCountOfAllCompletedTodosOnProject("46bb5ae4-dba6-41b1-b28e-e83b0c913347"));
+import { initialLoad } from "../components/scripts/html";
 
-const proj = JSON.parse(localStorage.getItem("projects"))[0];
-
-const x = createProjectCard(proj);
-renderTodos("9b1c9adc-dfb0-43aa-b525-1a3f23de74eb");
+initialLoad();
--- src/components/scripts/html.js
+++ src/components/scripts/html.js
@@ -1,6 +1,29 @@
-export const clearContent = function () {
-  const content = document.querySelector(".content");
+import { checkIfLocalStorageDataExists, getAllProjects } from "../../database/ls";
+import { createProjectCards } from "../../features/projects/scripts/renderProjects";
+
+export const clearContent = function (querySelector) {
+  const content = document.querySelector(querySelector);
   while (content.firstChild) {
     content.removeChild(content.firstChild);
   }
+};
+
+export const createBanner = function (mainText, subText) {
+  const aside = document.querySelector("aside");
+  const headingEl = document.createElement("h1");
+  const subTextEl = document.createElement("p");
+
+  headingEl.textContent = mainText;
+  subTextEl.textContent = subText;
+
+  clearContent("aside");
+  aside.append(headingEl, subTextEl);
+};
+
+export const initialLoad = function () {
+  checkIfLocalStorageDataExists();
+  createProjectCards(getAllProjects());
+  //check if we have data
+  //display data
+  //set up event listeners
 };
